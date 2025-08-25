9ddc88e0e6b5e1e558505163f789db8a3a4f1af8
Usaid <usaidokdev@gmail.com>
2025-08-25 18:17:23 +0530

    Asked from user

---
+++ Calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// int addition(){
+
+// }
+
+// int subtraction(){
+
+// }
+
+// int Multiplication(){
+
+// }
+
+// int devision(){
+
+// }
+int main(){
+    int num1,num2;
+    printf("Welcome to Smart Calculator!\n");
+    printf("Please enter your first number:");
+    scanf("%d",&num1);
+    printf("Please enter the second number:\n");
+    scanf("%d",&num2);
+    
+
+
+    return 0;
+}
