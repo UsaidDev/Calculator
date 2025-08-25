911aa2e44ffd89d5033cd4dd7908612e20e3c22c
Usaid <usaidokdev@gmail.com>
2025-08-25 19:37:18 +0530

    Calculator completed

--- Calculator.c
+++ Calculator.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 
-// int addition(){
-
-// }
-
-// int subtraction(){
-
-// }
-
-// int Multiplication(){
-
-// }
-
-// int devision(){
-
-// }
+void addition(int num1, int num2){
+    printf("Answer is:%d", num1+num2);
+}
+void subtraction(int num1,int num2){
+    printf("Answer is:%d",num1-num2);
+}
+void Multiplication(int num1,int num2){
+    printf("Answer is:%d",num1*num2);
+}
+void division(int num1,int num2){
+    printf("Answer is:%d",num1/num2);
+}
 int main(){
-    int num1,num2;
+    int num1, num2;
+    char choice;
     printf("Welcome to Smart Calculator!\n");
-    printf("Please enter your first number:");
-    scanf("%d",&num1);
-    printf("Please enter the second number:\n");
-    scanf("%d",&num2);
-    
+    printf("Enter two numbers:");
+    scanf("%d %d", &num1, &num2);
 
+    printf("Enter an operator(+,-,*,/):");
+    scanf(" %c", &choice); 
+    printf("%c\n", choice);
 
+    if (choice == '+') {
+        addition(num1,num2);
+    } else if (choice == '-') {
+        subtraction(num1,num2);
+    } else if (choice == '*') {
+        Multiplication(num1,num2);
+    } else if (choice == '/') {
+        if (num2 != 0) {
+            division(num1,num2);
+        } else {
+            printf("Error: Division by zero!\n");
+        }
+    } else {
+        printf("Invalid operator.\n");
+    }
+    printf("\nProgram finished!!");
     return 0;
 }
